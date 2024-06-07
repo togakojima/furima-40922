11b58e36bc1163e67b97e62113518eeae7b4be45
togakojima <togakojima1999@gmail.com>
2024-06-07 21:53:35 +0900

    userモデル単体テストコードの実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.last_name}
+    email {Faker::Internet.email}
+    password {"test1234"}
+    password_confirmation {password}
+    firstname_kanji {"太郎"}
+    lastname_kanji {"山田"}
+    firstname_kana {"タロウ"}
+    lastname_kana {"ヤマダ"}
+    birthday {"2000-01-01"}
+  end
+end
