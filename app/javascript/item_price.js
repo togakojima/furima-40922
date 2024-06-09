bcd3502c2d3e03d456efabac7fcab78aceb8e8a7
togakojima <togakojima1999@gmail.com>
2024-06-09 21:33:49 +0900

    販売手数料、販売利益の表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,9 @@
+const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = `${Math.floor(inputValue / 10).toLocaleString()}`;
+    const profitDom = document.getElementById("profit");
+    profitDom.innerHTML = `${Math.ceil(inputValue - inputValue / 10).toLocaleString()}`;
+  }); 
+
