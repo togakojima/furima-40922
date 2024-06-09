e791a7c5ccdf8a809025a0accb35621d4bf33ce8
togakojima <togakojima1999@gmail.com>
2024-06-09 20:10:04 +0900

    商品出品　エラー表示の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,12 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
+    validates :image, :title, :info
     validates :category_id, :status_id, :fee_id, :prefecture_id, :schedule_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :price, format: {with: /\A[0-9]+\z/, message: 'is not a number'}
   end
-
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, 
+                    less_than_or_equal_to: 9999999, message: "must be between 300 and 9999999"}, allow_blank: true
+  
 
 end
