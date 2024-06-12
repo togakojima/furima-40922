924c921e139d3f2aa6502868804aa3adacf7b8b5
togakojima <togakojima1999@gmail.com>
2024-06-12 13:09:57 +0900

    購入データの保存ができる

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,22 @@
 class PurchasesController < ApplicationController
   def index
-    
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @purchase = Purchase.create(purchase_params)
+    Address.create(address_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def purchase_params
+    params.permit(:item_id).merge(user_id: current_user.id)
+  end
+
+  def address_params
+    params.permit(:postcode, :prefecture_id, :city, :house_num, :building_name, :tel_num).merge(purchase_id: @purchase.id)
   end
 
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,8 @@
 class Address < ApplicationRecord
   belongs_to :purchase
+
+  with_options presence: true do
+    validates :city, :house_num
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  end
 end
