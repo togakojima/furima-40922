34a63c608d6620c8167effdc0a44f3dbdcf3c627
togakojima <togakojima1999@gmail.com>
2024-06-13 20:26:07 +0900

    クレジット決済機能、モデルテストコード実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,15 +4,18 @@ class PurchasesController < ApplicationController
   before_action :move_to_index, only: [:index, :create]
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @purchase_address = PurchaseAddress.new
   end
 
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -24,12 +27,21 @@ class PurchasesController < ApplicationController
   end
 
   def purchase_params
-    params.require(:purchase_address).permit(:postcode, :prefecture_id, :city, :house_num, :building_name, :tel_num).merge(item_id: params[:item_id], user_id: current_user.id)
+    params.require(:purchase_address).permit(:postcode, :prefecture_id, :city, :house_num, :building_name, :tel_num).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
   end
 
   def move_to_index
     if current_user.id == @item.user.id || @item.purchase.present?
       redirect_to root_path
     end
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: purchase_params[:token],
+        currency: 'jpy'
+      )
   end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,8 +1,9 @@
 class PurchaseAddress
   include ActiveModel::Model 
-  attr_accessor :postcode, :prefecture_id, :city, :house_num, :building_name, :tel_num, :item_id, :user_id
+  attr_accessor :postcode, :prefecture_id, :city, :house_num, :building_name, :tel_num, :item_id, :user_id, :token
 
   with_options presence: true do
+    validates :token
     validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city, :house_num, :user_id, :item_id
