a6637fc91cd28e0962178e9164446d3ed95739dd
togakojima <togakojima1999@gmail.com>
2024-06-13 21:30:15 +0900

    コード修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,7 +47,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if current_user == @item.user || @item.purchase.present?
+    if current_user.id != @item.user.id || @item.purchase.present?
      redirect_to root_path
     end
   end
